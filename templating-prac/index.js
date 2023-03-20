45155f325bce94d427e216b16bf00dfbe0860d38
annapaula_d <annapaula.desouza1@gmail.com>
2023-03-20 13:31:53 -0400

    Initial commit

--- templating-prac/index.js
+++ templating-prac/index.js
@@ -8,6 +8,15 @@ app.set('views', path.join(__dirname, '/views'));
 app.get('/', (req, res) => {
     res.render('home')
 })
+
+app.get('/cats', (req, res) => {
+    const cats = ['Blue', 'Rocket', 'Monty', 'Stephanine', 'Winston']
+    res.render('cats', { cats })
+})
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render('subreddit', { subreddit });
+})
 app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1;
     res.render('random', { rand: num })
