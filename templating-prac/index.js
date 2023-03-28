3223b7b85b66d7bf5ad9a9aaaa90c1e65afbdd94
annapaula_d <annapaula.desouza1@gmail.com>
2023-03-28 16:17:56 -0400

    Initial commit

--- templating-prac/index.js
+++ templating-prac/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
@@ -15,8 +19,16 @@ app.get('/cats', (req, res) => {
 })
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
-    res.render('subreddit', { subreddit });
+    const data = redditData[subreddit];
+    if (data) {
+        res.render('subreddit', { ...data });
+    }
+    else {
+        res.render('notfound', { subreddit })
+    }
+
 })
+
 app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1;
     res.render('random', { rand: num })
