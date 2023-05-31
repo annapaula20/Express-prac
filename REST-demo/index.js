29c124ad44d74725a761158a6e829955dc367b49
annapaula_d <annapaula.desouza1@gmail.com>
2023-05-31 12:20:06 -0400

    Initial commit

--- REST-demo/index.js
+++ REST-demo/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 
+
+app.use(express.urlencoded({ extended: true })) // code line is needed to run either codes or function on every single request.
+
+app.use(express.json())// parses incoming requests with JSON data instead of url.
 app.get('/tacos', (req, res) => {
     res.send('Get/ tacos response')
 })
 
 app.post('/tacos', (req, res) => {
-    res.send('POST /tacos response')
+    const { meat, qty } = req.body;
+    res.send(`Ok, here are your ${qty} ${meat}`)
 })
 app.listen(3000, () => {
-    console.log('on port 3000');
+    console.log('on port 3000!');
 })
 
